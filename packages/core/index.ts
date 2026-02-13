3c5d0deef6309a5dc0036fd6a1df11ab61dd16a7
btmxh <ngoduyanh.chip@gmail.com>
2026-02-13 18:44:51 +0900

    refactor: get rid of extra index.ts

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,1 +1,6 @@
-export * from "./src/index";
+export * from "./src/math";
+export * from "./src/data";
+export * from "./src/utils";
+export * from "./src/process";
+export * from "./src/extension";
+export * from "./src/meta-helpers";
--- packages/core/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./math";
-export * from "./data";
-export * from "./utils";
-export * from "./process";
-export * from "./extension";
-export * from "./meta-helpers";
-
-// internal package entrypoint
